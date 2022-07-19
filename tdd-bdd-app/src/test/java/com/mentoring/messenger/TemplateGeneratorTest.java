4b9bfdc3ffb13199d01b82286656873ebf3eaf80
Yury Kunitski <yury_kunitski@epam.com>
2022-07-19 13:02:03 +0200

    Add parameterized and dynamic tests.

--- tdd-bdd-app/src/test/java/com/mentoring/messenger/TemplateGeneratorTest.java
+++ tdd-bdd-app/src/test/java/com/mentoring/messenger/TemplateGeneratorTest.java
@@ -1,32 +1,60 @@
 package com.mentoring.messenger;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
+import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
+import org.junit.jupiter.api.function.Executable;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class TemplateGeneratorTest {
 
   private final TemplateGenerator generator = new TemplateGenerator();
 
-  @Test
-  void replaceSubjectHappyPath() {
-    String expectedMessage = "Some text: #{ValueName}";
-    Map<String, String> values = Collections.singletonMap("value", "#{ValueName}");
+  @ParameterizedTest
+  @MethodSource("getValidValues")
+  void generateTemplateHappyPath(String value) {
+    String expectedMessage = "Some text: " + value;
+    Map<String, String> values = Collections.singletonMap("value", value);
     String actualMessage = generator.generateTemplate(values);
 
     assertEquals(expectedMessage, actualMessage);
   }
 
-  @Test
-  void replaceSubjectUnHappyPath() {
-    Map<String, String> values = Collections.singletonMap("value", "");
+  @ParameterizedTest
+  @ValueSource(strings = {"", " "})
+  void generateTemplateUnHappyPath(String value) {
+    Map<String, String> values = Collections.singletonMap("value", value);
 
     IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
         () -> generator.generateTemplate(values));
     assertEquals("Value is not provided.", exception.getMessage());
   }
 
+  @TestFactory
+  Collection<DynamicTest> generateTemplateDynamicTest() {
+    String expectedMessage = "Some text: valueName";
+    String key = "value";
+
+    Executable executableByInvalidValue = () -> generator.generateTemplate(
+        Collections.singletonMap(key, ""));
+
+    return List.of(
+        DynamicTest.dynamicTest("Generate template by valid value",
+            () -> assertEquals(expectedMessage,
+                generator.generateTemplate(Collections.singletonMap(key, "valueName")))),
+        DynamicTest.dynamicTest("Generate template by invalid value",
+            () -> assertThrows(IllegalArgumentException.class, executableByInvalidValue)));
+  }
+
+  private static List<String> getValidValues() {
+    return List.of("#{ValueName}", "valueName", "0");
+  }
 
 }
