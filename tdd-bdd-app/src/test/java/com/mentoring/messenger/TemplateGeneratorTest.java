b082a14d4acf43d83722b6fb932bb92dc4992c99
Yury Kunitski <yury_kunitski@epam.com>
2022-07-19 14:03:06 +0200

    Implemented meta annotations and filtering.

--- tdd-bdd-app/src/test/java/com/mentoring/messenger/TemplateGeneratorTest.java
+++ tdd-bdd-app/src/test/java/com/mentoring/messenger/TemplateGeneratorTest.java
@@ -1,11 +1,13 @@
 package com.mentoring.messenger;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestFactory;
 import org.junit.jupiter.api.function.Executable;
@@ -51,6 +53,46 @@ class TemplateGeneratorTest {
                 generator.generateTemplate(Collections.singletonMap(key, "valueName")))),
         DynamicTest.dynamicTest("Generate template by invalid value",
             () -> assertThrows(IllegalArgumentException.class, executableByInvalidValue)));
+  }
+
+  @Test
+  @Tag("include")
+  void generateTemplateHappyPathFilteredIncluded() {
+    String expectedMessage = "Some text: #{valueName}";
+    Map<String, String> values = Collections.singletonMap("value", "#{valueName}");
+    String actualMessage = generator.generateTemplate(values);
+
+    assertEquals(expectedMessage, actualMessage);
+  }
+
+  @Exclude
+  void generateTemplateHappyPathFilteredExcluded() {
+    String expectedMessage = "Some text: #{valueName}";
+    Map<String, String> values = Collections.singletonMap("value", "#{valueName}");
+    String actualMessage = generator.generateTemplate(values);
+
+    assertEquals(expectedMessage, actualMessage);
+  }
+
+  @Include
+  void generateTemplateUnHappyPathFilteredIncluded() {
+    Map<String, String> values = Collections.singletonMap("value", "");
+
+    IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+        () -> generator.generateTemplate(values));
+
+    assertEquals("Value is not provided.", exception.getMessage());
+  }
+
+  @Test
+  @Tag("exclude")
+  void generateTemplateUnHappyPathFilteredExcluded() {
+    Map<String, String> values = Collections.singletonMap("value", "");
+
+    IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+        () -> generator.generateTemplate(values));
+
+    assertEquals("Value is not provided.", exception.getMessage());
   }
 
   private static List<String> getValidValues() {
