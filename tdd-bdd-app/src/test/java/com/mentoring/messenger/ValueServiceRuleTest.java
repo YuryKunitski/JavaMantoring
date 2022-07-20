dc2c965c908b1c0279387eb5757774322b577ed5
Yury Kunitski <yury_kunitski@epam.com>
2022-07-20 12:20:44 +0200

    Added test with TemporaryFolder rule.

---
+++ tdd-bdd-app/src/test/java/com/mentoring/messenger/ValueServiceRuleTest.java
@@ -0,0 +1,38 @@
+package com.mentoring.messenger;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertLinesMatch;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Collections;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+public class ValueServiceRuleTest {
+
+  private final ValueService valueService = new ValueService();
+
+  @Rule
+  public TemporaryFolder tmpFolder = TemporaryFolder.builder().assureDeletion().build();
+
+  @Test
+  public void saveValueIntoFileTempDirByRule() throws IOException {
+    String fileName = "testInputFile";
+    File testFile = tmpFolder.newFile(fileName + ".txt");
+
+    String expectedText = "Some value: #{ValueName}";
+
+    valueService.saveValueIntoFile(String.format("%s/%s", testFile.getParent(), fileName),
+        expectedText);
+
+    assertAll(
+        () -> assertTrue(Files.exists(testFile.toPath()), "File should exist"),
+        () -> assertLinesMatch(Collections.singletonList(expectedText),
+            Files.readAllLines(Path.of(testFile.getPath()))));
+  }
+}
